55d196792f2f5c194d9691ee1b570a82a1b18134
DarkArex <147281458+DarkArex@users.noreply.github.com>
2023-10-08 14:32:19 +0500

    Add files via upload

---
+++ program/9.1.py
@@ -0,0 +1,21 @@
+import time
+
+def timing_decorator(function):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = function(*args, **kwargs)
+        end_time = time.time()
+        print(f"Время выполнения функции {function.__name__}: {end_time - start_time} секунд")
+        return result
+    return wrapper
+
+@timing_decorator
+def fibonacci():
+    fib1 = fib2 = 1
+    for i in range(2, 200):
+        fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end=' ')
+
+if __name__ == '__main__':
+    fibonacci()
+
